9bfbf9ffd6143b3659cf43b6a964185d0ff8761f
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2023-05-30 22:35:00 +0700

    Update asdasdasdasccc.lua

--- asdasdasdasccc.lua
+++ asdasdasdasccc.lua
@@ -1,7 +1,8 @@
+local env = getgenv()
 local dotehookold
 dotehookold = hookfunction(require(game:GetService("ReplicatedStorage").Notification).new,function(...)
     local Args = {...}
-    if Args[1] and string.find(Args[1],"killing") and getgenv().Setting["Webhook"]["Enabled"]  then 
+    if Args[1] and string.find(Args[1],"killing") and env.Setting["Webhook"]["Enabled"]  then 
         local Tiltebounty = string.split(Args[1],"killing")
         sendmsgbounty(getgenv().Setting["Webhook"]["Url Webhook"],string.gsub(Tiltebounty[1],"%D",""),Tiltebounty[2])
     end
