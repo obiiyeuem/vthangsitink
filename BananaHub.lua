785886e27d5a37b4f34d8de4aa599577a56d76ee
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2023-10-23 17:46:28 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -6,5 +6,5 @@ if game.PlaceId == 14433762945 then
 elseif game.PlaceId == 13772394625 or game.PlaceId == 14732610803 or game.PlaceId == 14915220621 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BladeBall.lua"))()
 else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BloxFruits.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-Beta.lua"))()
 end
