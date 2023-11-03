df05852ab766139365aa8aa72a374300b68e4747
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2023-11-04 01:58:59 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -1,26 +1,12 @@
 
 repeat task.wait() until game:IsLoaded()
-
-local function ReturnBF(U)
-return {getgenv().OldBanana, U == 994732206}
-end
-
-local function Run(A1)
-loadstring(game:HttpGet(A1))()
-end
-
-local Games = {
-[4986566693] = "https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/ACS.lua",
-[994732206] = {[true] = "https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/OldBanana.lua", [false] = "https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-Beta.lua"},
-[4777817887] = "https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BladeBall.lua"
-}
-
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-
-local Old = not not ReturnBF(UniverseID)[1] and ReturnBF(UniverseID)[1] or false
-
-if ReturnBF(UniverseID)[2] == false then
-Run(Games[UniverseID])
+local TablePlace = {7449423635,2753915549,4442272183}
+if table.find(TablePlace,game.PlaceId) then 
+    if getgenv().BananaOld then 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/OldBanana.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/OldBanana.lua"))()
+    end
 else
-Run(Games[UniverseID][Old])
+    game.Players.LocalPlayer:Kick("not Support")
 end
