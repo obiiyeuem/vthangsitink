50035dfcd216be5488ea5ea4a35ab2e043a613d8
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2023-11-04 11:32:41 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -2,7 +2,7 @@
 repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 if table.find(TablePlace,game.PlaceId) then 
-    if getgenv().BananaOld then 
+    if getgenv().OldBanana then 
         loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/OldBanana.lua"))()
     else
         loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-Beta.lua"))()
