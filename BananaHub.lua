7c73ad8918f4a3bb1230149e0e77dfaa1eaf1867
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2024-06-16 02:53:55 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 if not getgenv().NewBanana then 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-Beta.lua"))()
+	game.Players.LocalPlayer:Kick("Use new banana goto #command-bot use slash /script get new script)
 else
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
 end
