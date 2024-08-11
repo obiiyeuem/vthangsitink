6d1a5834aae459d2bfd37f72f25b049016bba847
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2024-08-11 15:20:52 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 1537690962 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BSS-BananaCat.lua"))()
+elseif game.PlaceId == 10260193230 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/Seahuhu-BananaCat.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
 end
