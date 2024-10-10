b355c03924e0d0b19af1287368c686f45fcb3d52
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2024-10-10 19:47:56 +0700

    Update BananaCat-BountyEz.lua

--- BananaCat-BountyEz.lua
+++ BananaCat-BountyEz.lua
@@ -1,1 +1,2 @@
-https://raw.githubusercontent.com/RedGamer12/TNNP-SYSTEM/refs/heads/main/client/BloxFruit/Bounty_Rewrite-obfuscated.lua"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RedGamer12/TNNP-SYSTEM/refs/heads/main/client/BloxFruit/Bounty_Rewrite-obfuscated.lua"))()
+
