ab2fffdd6fd53cc5bf738ad3c2e79b8759dc1ff0
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2025-03-26 08:28:05 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -21,6 +21,8 @@ elseif UniverseID == 5844593548  then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/AnimeReborn.lua"))()
 elseif UniverseID == 6325068386 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/Bluelock.lua"))()
+elseif game.PlaceId == 10260193230 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/Arise.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/AV-BananaCat.lua"))()
 end
