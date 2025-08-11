a6f4b4b3a0f26d213247a362a8cbde713220ed71
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2025-08-11 15:00:58 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -8,7 +8,11 @@ if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
 elseif game.PlaceId == 10260193230 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/Seahuhu-BananaCat.lua"))()
 elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
+    if getgenv().NewUI then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BNNC-BFNew.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
+    end
 elseif game.PlaceId == 4520749081 or  game.PlaceId == 6381829480 or game.PlaceId == 15759515082 or game.PlaceId == 5931540094 then 
     local start = tick()
     repeat task.wait() until game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("DataLoaded") and game.Players.LocalPlayer:FindFirstChild("DataLoaded").Value
@@ -32,3 +36,4 @@ elseif UniverseID == 7709344486 then
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/AV-BananaCat.lua"))()
 end
+
