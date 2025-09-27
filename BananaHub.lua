fe2672af8005bce5bb90602d6c837a8d0a84941f
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2025-09-27 22:36:50 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -7,7 +7,7 @@ if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
     end
 elseif game.PlaceId == 10260193230 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/Seahuhu-BananaCat.lua"))()
-elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
+elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
 elseif game.PlaceId == 4520749081 or  game.PlaceId == 6381829480 or game.PlaceId == 15759515082 or game.PlaceId == 5931540094 then 
     local start = tick()
@@ -32,5 +32,6 @@ elseif UniverseID == 7709344486 then
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/AV-BananaCat.lua"))()
 end
+
 
 
