88ef76278b6465926dd508f708f8ab9113e7fc73
obii#3012 <74485985+obiiyeuem@users.noreply.github.com>
2025-12-13 06:46:17 +0700

    Update BananaHub.lua

--- BananaHub.lua
+++ BananaHub.lua
@@ -29,9 +29,12 @@ elseif UniverseID  == 7436755782 then
      loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/GrowAGraden.lua"))()
 elseif UniverseID == 7709344486 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-Brainrot.lua"))()
+elseif UniverseID == 7671049560 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/theforge.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/AV-BananaCat.lua"))()
 end
+
 
 
 
